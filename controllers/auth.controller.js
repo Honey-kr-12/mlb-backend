bc66c3884c4fd58b4c0e442cf76f40e4958b6378
shivam <shivammahrolia@gmail.com>
2024-12-26 19:47:33 +0530

    ok

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -184,8 +184,9 @@ export const login = async (req, res) => {
 
 
 export const logout = (req, res) => {
-  console.log("okok");
   try {
+    console.log("okok");
+    
     res.cookie("jwt", "", { maxAge: 0 });
     res.status(200).json({ message: "Logged out successfully" });
   } catch (error) {
