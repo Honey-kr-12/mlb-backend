6c4db43a7957e3dc88a1b33b1303cbb3189540b2
shivam <shivammahrolia@gmail.com>
2024-12-29 02:43:28 +0530

    ok

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ const corsOptions = {
   app.use(cors(corsOptions));
 
 
-const PORT = 5000; // You can change the port as needed
+const PORT = 5000; // You can change the port as needed okay
 
 // Middleware to parse JSON
 app.use(express.json());
